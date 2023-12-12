6cb82a571830b9f848869608ad38e8095eabe4bc
Saakshi More <sm9890@nyu.edu>
2023-12-12 11:47:41 -0500

    icon

--- src/streamlit.py
+++ src/streamlit.py
@@ -133,7 +133,7 @@ def get_conflict_plot_data():
     full_map = gpd.GeoDataFrame.from_file(full_map_path)
     return df, israel_map, pal_map, full_map
 
-st.set_page_config(page_title="Media Bias in Israel-Palestine Coverage", page_icon="🍉", layout="wide")
+st.set_page_config(page_title="Media Bias in Israel-Palestine Coverage", page_icon="📈", layout="wide")
 
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
